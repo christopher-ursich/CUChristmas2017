dd0233b8600edf52f2d97328a965ae71fe416db9
Christopher Ursich <christopher.ursich@ursichfamily.org>
2017-12-13 16:21:08 -0500

    WIP: Can successfully get Google account info from GoogleSignInAccount

--- app/src/main/java/org/ursichfamily/apps/cuchristmas2017/MainActivity.java
+++ app/src/main/java/org/ursichfamily/apps/cuchristmas2017/MainActivity.java
@@ -3,6 +3,7 @@ package org.ursichfamily.apps.cuchristmas2017;
 import android.Manifest;
 import android.accounts.Account;
 import android.accounts.AccountManager;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.res.Resources;
 import android.support.v4.app.ActivityCompat;
@@ -14,12 +15,17 @@ import android.view.View;
 import android.widget.TextView;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private final int PERM_REQUEST_ID_GET_ACCOUNTS = 24601;
+    private final int PERM_REQUEST_ID_GET_ACCOUNTS = 0;
+    private final int RC_SIGN_IN = 1;
+    GoogleSignInClient mGoogleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,14 +50,14 @@ public class MainActivity extends AppCompatActivity {
                     + " type:" + accounts[i].type
                     + " creator: " + accounts[i].CREATOR
                     + " name:" + accounts[i].name
-                    ;
+            ;
         }
         //String s = "granted. num of accts = " + accounts.length + "\naccounts[0].name = " + accounts[0].name;
         tv.setText(s);
 
         // Configure sign-in to request the user's ID and basic profile (included in DEFAULT_SIGN_IN).
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).build();
-        GoogleSignInClient mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
     }
 
     @Override
@@ -59,8 +65,48 @@ public class MainActivity extends AppCompatActivity {
         switch (v.getId()) {
             case R.id.sign_in_button:
                 signIn();
-                break;
-            // ...
+        }
+    }
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    // The GoogleSignInAccount object contains information about the signed-in user, such as the user's name.
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+            // Signed in successfully, show authenticated UI.
+            Log.i("no_tag", "sign-in successful");
+            TextView tv = findViewById(R.id.words);
+            String s = "";
+            s += "DisplayName: " + account.getDisplayName();
+            s += "\nEmail: " + account.getEmail();
+            s += "\nFamilyName: " + account.getFamilyName();
+            s += "\nGivenName: " + account.getGivenName();
+            s += "\nId: " + account.getId();
+            s += "\nPhotoUrl: " + account.getPhotoUrl();
+            tv.setText(s);
+            Log.i("no_tag", s);
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w("no_tag", "signInResult:failed code=" + e.getStatusCode());
         }
     }
 
